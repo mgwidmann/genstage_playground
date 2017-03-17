ff96e54deefcacde9da2a56c8b0b4ff4a9ebccb5
Matt Widmann <mgwidmann@gmail.com>
2017-03-17 12:07:53 -0400

    Add flow example.

--- lib/genstage_playground.ex
+++ lib/genstage_playground.ex
@@ -1,5 +1,6 @@
 defmodule GenstagePlayground do
   use Application
+  require Integer
 
   # See http://elixir-lang.org/docs/stable/elixir/Application.html
   # for more information on OTP Applications
@@ -24,5 +25,25 @@ defmodule GenstagePlayground do
     GenstagePlayground.Consumer.register()
     GenstagePlayground.Consumer.begin()
     GenstagePlayground.Consumer.wait()
+  end
+
+  def flow() do
+    # Producer generating numbers
+    0
+    |> Stream.iterate(fn i ->
+      IO.puts "P Producing a new number"
+      i + 1
+    end)
+    |> Stream.take(100_000)
+    |> Flow.from_enumerable()
+    |> Flow.partition()
+    # Producer-Consumer filtering even numbers
+    |> Flow.filter(fn i ->
+      IO.puts "PC Filtering"
+      Integer.is_even(i)
+    end)
+    |> Flow.partition()
+    # Consumer printing out numbers
+    |> Flow.each(fn i -> IO.puts "C Processing #{i}" end)
   end
 end
